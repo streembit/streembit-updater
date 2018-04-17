c2ec05d8636b4f4126a34d52a42db6f65c028a41
AlDoo <alex.doo.gm@gmail.com>
2018-04-17 13:51:19 +0300

    restart pm2 on cli update

--- github-updater/githook_master.js
+++ github-updater/githook_master.js
@@ -3,7 +3,7 @@ const { execSync } = require('child_process');
 
 const config = require('./config');
 
-let stdout = '';
+let stdout = '', cmd = '';
 
 stdout = execSync('whereis git');
 if (stdout.toString().length < 1) {
@@ -17,16 +17,27 @@ gith({
     const repo = payload.repo.replace(/^streembit\//, '');
     if (config.hasOwnProperty(repo)) {
         try {
-            const cmd = `
+            cmd = `
                 cd ${config[repo]["path"]}        
                 git reset --hard HEAD^
                 git checkout ${payload.branch}
                 ${config[repo]["command"]}
             `;
-            
+
             stdout = execSync(cmd);
 
             if (!/error/i.test(stdout)) {
+                if (repo === 'streembit-cli') {
+                    stdout = execSync('pm2 show streembit | grep "script args" | awk \'{split($0,a,"│"); print a[3]}\' | cut -d " " -f 2');
+                    const pwd = stdout.toString().trim();
+                    cmd = `
+                       cd ${config[repo]["path"]}
+                       pm2 delete streembit
+                       node pm2start ${pwd}
+                    `;
+
+                    stdout = execSync(cmd);
+                }
                 if (config[repo]["nodes"].length) {
                     // ToDo: update configured nodes
                 }
