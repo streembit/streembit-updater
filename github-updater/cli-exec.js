f3f349d20cf50e9cb5195e4b1a658dc25ee71b37
AlDoo <alex.doo.gm@gmail.com>
2018-05-10 11:35:36 +0300

    default branch in cli-exec

--- github-updater/cli-exec.js
+++ github-updater/cli-exec.js
@@ -5,11 +5,21 @@ const config = require('./config');
 
 let stdout = '', cmd = '';
 
+// since we're running both branches in the same folder
+// checkout to main branch after each pull
+/*
+ - develop : staging
+ - master : prod
+*/
+const MAIN_BRANCH = 'develop';
+
 exports.exec = function exec(repo) {
     stdout = execSync('pm2 show streembit | grep "script args" | awk \'{split($0,a,"│"); print a[3]}\' | cut -d " " -f 2');
     const pwd = stdout.toString().trim();
+
     cmd = `
         cd ${config[repo]["path"]}
+        git checkout ${MAIN_BRANCH}
         pm2 delete streembit
         node pm2start ${pwd}
     `;
